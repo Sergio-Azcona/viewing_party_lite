3e63596e8536acbaf35d848731b913766caf1512
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2023-01-31 22:11:09 -0500

    test/feat: landing page complete

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -10,21 +10,30 @@ RSpec.describe "welcome index page" do
     visit(root_path)
   end
 
-  it 'displays the app name, existing user section, create user button, and link to the home page' do
-    save_and_open_page
+  describe 'landing page' do
+    describe 'displays the app name, existing user section' do
+      it "displays links to existing users' dashboard within the Existing Users section" do
+        # save_and_open_page
+        expect(page).to have_content("Viewing Party")
+        expect(page).to have_content("Existing Users")
 
-    expect(page).to have_content("Viewing Party")
-    expect(page).to have_content("Existing Users")
-    expect(page).to have_link("Home", href: root_path)
-    expect(page).to have_button("Create a New User")
-  end
+        within("#existing_users") do
+          expect(page).to have_link("User 1", href: user_path(@u1.id))
+          expect(page).to have_link("Friendly 2", href: user_path(@u2.id))
+          expect(page).to have_link("Chill 3", href: user_path(@u3.id))
+          expect(page).to have_link("Bull 4", href: user_path(@u4.id))
+        end
+      end
+    end
+    
+    it 'has buttons to create a new user and a link to the home page which takes users to the respective pages' do
+      expect(page).to have_link("Home", href: root_path)
+      click_link("Home")
+      expect(current_path).to eq(root_path)
 
-  it "displays links to existing users' dashboard within the Existing Users section" do
-    within("#existing_users") do
-      expect(page).to have_link("User 1", href: user_path(@u1.id))
-      expect(page).to have_link("Friendly 2", href: user_path(@u2.id))
-      expect(page).to have_link("Chill 3", href: user_path(@u3.id))
-      expect(page).to have_link("Bull 4", href: user_path(@u4.id))
+      expect(page).to have_button("Create a New User")
+      click_button("Create a New User")
+      expect(current_path).to eq(new_user_path)
     end
   end
 end
